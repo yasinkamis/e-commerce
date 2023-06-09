f69f0dec9de4e402e45a2b4392f1b6714686b17b
ahmet <aenes.tanis@gmail.com>
2023-06-10 01:18:27 +0300

    faq

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Signup from "../components/Signup/Signup";
 
 const SignupPage = () => {
@@ -8,15 +8,15 @@ const SignupPage = () => {
   const { isAuthenticated } = useSelector((state) => state.user);
 
   useEffect(() => {
-    if(isAuthenticated === true){
+    if (isAuthenticated === true) {
       navigate("/");
     }
-  }, [])
+  }, []);
   return (
     <div>
-        <Signup />
+      <Signup />
     </div>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
